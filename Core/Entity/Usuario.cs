5fde89f77f1b84a7c118e070fc0c41601c7d7096
Taina Alecrim <tai.estudos@gmail.com>
2025-09-30 22:51:00 -0300

    criação tabela usuario

---
+++ Core/Entity/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entity
+{
+    public class Usuario : EntityBase
+    {
+        public required string Nome { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string? Email { get; set; }
+        public string? NickName { get; set; }
+        public int SenhaHash { get; set; }
+        public DateTime? UsuarioCriacao { get; set; }
+
+    }
+}
---
+++ Infrastructure/Repository/Configurations/UsuarioConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Core.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace Infrastructure.Repository.Configurations
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+            builder.HasKey(p => p.Guid);
+            builder.Property(p => p.Guid).HasColumnType("int").ValueGeneratedNever().UseIdentityColumn();
+            builder.Property(p => p.DataNascimento).HasColumnType("DATETIME").IsRequired();
+            builder.Property(p => p.Nome).HasColumnType("STRING").IsRequired();
+            builder.Property(p => p.Email).HasColumnType("STRING");
+            builder.Property(p => p.SenhaHash).HasColumnType("STRING");
+            builder.Property(p => p.UsuarioCriacao).HasColumnType("DATETIME").IsRequired();
+        }
+    }
+}
